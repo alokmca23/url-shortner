f3530142e00d8133e784923b71ce3639178a4712
Alok <alokkumarbahs7070@gmail.com>
2024-09-01 11:02:42 +0530

    mongodb connection

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const urlRoutes = require("./routes/url");
 const staticRoute = require("./routes/staticRouter");
 const {connectMongoDb} = require("./connection");
+require('dotenv').config()
 
 const app = express();
 const PORT = 8001;
@@ -10,7 +11,8 @@ const PORT = 8001;
 app.set("view engine","ejs");
 app.set("views",path.resolve("./views"));
 
-connectMongoDb("mongodb://localhost:27017/short-url")
+// connectMongoDb("mongodb://localhost:27017/short-url")
+ connectMongoDb(process.env.DATABASE_URL)
 .then(() => console.log("MongoDb connected!"));
 //Middleware
 app.use(express.json());
